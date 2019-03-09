aaaaebb834ee41ba8c7eac5ac051e7db5ef7ae7c
Ellen Pu <ellenpu251@gmail.com>
2019-03-09 22:44:17 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,18 +1,18 @@
 def position_taken?(board,index)
   index = index - 1
   if(board[index] == " " || board[index] == nil || board[index] == "")
-    return false
-  else
     return true
+  else
+    return false
   end
 end
 
 def valid_move?(board,index)
-  if(position_taken?(board,index) == false)
+  if(position_taken?(board,index) == true)
     if(index >= 1 && index <= 9)
-      return true
-    else
       return false
+    else
+      return true
     end
   else
     return false
