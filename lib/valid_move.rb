6a6dcd1638797a61ee307282deaf12c002ef97b3
Ellen Pu <ellenpu251@gmail.com>
2019-03-09 22:30:22 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,9 +1,9 @@
 def position_taken?(board,index)
-<<<<<<< HEAD
+#<<<<<<< HEAD
   if(board[index] == " " || board[index] == nil || board[index] == "")
 =======
   if(board[index] == " " || board[index] == "")
->>>>>>> 6548f7a7da42b0817a7763734afc4a58310916b5
+#>>>>>>> 6548f7a7da42b0817a7763734afc4a58310916b5
     return false
   else
     return true
